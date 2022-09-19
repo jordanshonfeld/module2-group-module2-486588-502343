163517212e9456280f20f27959e4ca2929477faf
Jinying Guan <g.jinying@wustl.edu>
2022-09-19 01:30:19 -0500

    comment

--- fileSharing.php
+++ fileSharing.php
@@ -4,6 +4,7 @@
         <title>Module2 group project</title>
     </head>
     <body>
+        <!--enter username to log in-->
         <form name = "input" action = "fileSharing.php" method = "get">
             <label for="userName">Enter your user name here: </label>
             <input type="text" id="userName" name="userName">
@@ -16,12 +17,12 @@
 $h = fopen("/srv/uploads/userNames/users.txt", "r");
 $input_user_name = $_GET['userName']; 
 $is_user = false;
-
+//check if the input username exist in user.txt
 while( !feof($h) ){
     $user_name = trim(fgets($h));
     $is_user = $is_user | ($input_user_name == $user_name);
 }
-
+//username exists, successfully log in
 if($is_user && isset($input_user_name)) {
     session_start();
     echo '<h1>' .$input_user_name .' files</h1>';
@@ -29,7 +30,7 @@ if($is_user && isset($input_user_name)) {
     header("Location: userInterface.php");
 
 }
-
+//username does not exist
 if(!$is_user && isset($input_user_name)) {
     echo '<br> <h1>Please enter a valid user name</h1>';
 }
--- userInterface.php
+++ userInterface.php
@@ -3,6 +3,7 @@ session_start();
 $input_user_name = $_SESSION['userName'];
 echo '<h1> Hello ' . $input_user_name . '</h1>';
 $directory_name = '/srv/uploads/' . $input_user_name . "Files";
+//view the list of files under this user
 echo '<form name = "input" action = "viewFiles.php" method = "get">';
 if (is_dir($directory_name)) {
     if ($dh = opendir($directory_name)) {
@@ -18,6 +19,7 @@ if (is_dir($directory_name)) {
 }
 echo '<input type ="submit" value="View"/>
 </form>';
+//upload files
 $upload_format = '<form enctype="multipart/form-data" action="uploader.php" method="POST">
 <p>
     <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
@@ -27,11 +29,13 @@ $upload_format = '<form enctype="multipart/form-data" action="uploader.php" meth
     <input type="submit" value="Upload File" />
 </p>
 </form>';
+//delete files
 $delete_format ='  <form name = "input" action = "delete.php" method = "get">
 <label for="file_to_delete">type the file name to be deleted here:</label>
 <input type="text" name="file_to_delete" id="file_to_delete"/>
 <input type="submit" value="Delete File" />
 </form>';
+//change username
 $change_username ='  <form name = "input" action = "changeUsername.php" method = "get">
 <label for="new_name">change the user name:</label>
 <input type="text" name="new_name" id="new_name"/>
